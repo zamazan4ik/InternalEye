8bb11bde5909fb86c86d73ce1d4f351b24e4b999
zamazan4ik <zamazan4ik@tut.by>
2016-05-04 19:21:11 +0300

    Remove trash

--- build/build-Release/CMakeFiles/InternalEye_automoc.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "InternalEye_automoc.cpp"
-  "CMakeFiles/InternalEye_automoc"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/InternalEye_automoc.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
